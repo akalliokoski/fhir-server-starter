62b6cd8c63399cfe989c3cb8c4507b8b0a9beba3
Antti Kalliokoski <antti.a.kalliokoski@gmail.com>
2019-02-20 13:42:57 +0200

    Use embedded MongoDB

--- src/main/java/uk/nhs/careconnect/fhirStarter/providers/PatientProvider.java
+++ src/main/java/uk/nhs/careconnect/fhirStarter/providers/PatientProvider.java
@@ -4,14 +4,11 @@ import ca.uhn.fhir.context.FhirContext;
 
 import ca.uhn.fhir.rest.annotation.*;
 import ca.uhn.fhir.rest.api.MethodOutcome;
-import ca.uhn.fhir.rest.param.DateParam;
 import ca.uhn.fhir.rest.param.DateRangeParam;
 import ca.uhn.fhir.rest.param.StringParam;
 import ca.uhn.fhir.rest.param.TokenParam;
 import ca.uhn.fhir.rest.server.IResourceProvider;
 
-import ca.uhn.fhir.rest.server.exceptions.InternalErrorException;
-import org.bson.types.ObjectId;
 import org.hl7.fhir.dstu3.model.IdType;
 import org.hl7.fhir.dstu3.model.OperationOutcome;
 import org.slf4j.Logger;
@@ -24,11 +21,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
-
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -49,7 +41,6 @@ public class PatientProvider implements IResourceProvider {
 
     @Create
     public MethodOutcome createPatient(HttpServletRequest theRequest, @ResourceParam Patient patient) {
-
         log.debug("Create Patient Provider called");
 
         MethodOutcome method = new MethodOutcome();
@@ -68,36 +59,26 @@ public class PatientProvider implements IResourceProvider {
         }
 
         log.debug("called create Patient method");
-
         return method;
     }
 
-    /*
     @Read
     public Patient readPatient(HttpServletRequest request, @IdParam IdType internalId) {
-
-        Patient patient = patientDao.read(ctx,internalId);
-
+        Patient patient = patientDao.read(ctx, internalId);
         return patient;
     }
-*/
 
-    /*
     @Search
     public List<Resource> searchPatient(HttpServletRequest request,
-                                        @OptionalParam(name= Patient.SP_BIRTHDATE) DateRangeParam birthDate,
+                                        @OptionalParam(name = Patient.SP_BIRTHDATE) DateRangeParam birthDate,
                                         @OptionalParam(name = Patient.SP_FAMILY) StringParam familyName,
-                                        @OptionalParam(name= Patient.SP_GENDER) StringParam gender ,
-                                        @OptionalParam(name= Patient.SP_GIVEN) StringParam givenName ,
+                                        @OptionalParam(name = Patient.SP_GENDER) StringParam gender,
+                                        @OptionalParam(name = Patient.SP_GIVEN) StringParam givenName,
                                         @OptionalParam(name = Patient.SP_IDENTIFIER) TokenParam identifier,
-                                        @OptionalParam(name= Patient.SP_NAME) StringParam name
-            , @OptionalParam(name = Patient.SP_RES_ID) TokenParam resid
+                                        @OptionalParam(name = Patient.SP_NAME) StringParam name
 
     ) {
-        List<Resource> results = patientDao.search(ctx,birthDate,familyName,gender,givenName,identifier,name);
-
+        List<Resource> results = patientDao.search(ctx, birthDate, familyName, gender, givenName, identifier, name);
         return results;
     }
-    */
-
 }
